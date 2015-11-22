8c62eb357225248919eb4924295371ac9bbc920f
Zaviša Nikač <znikac@srce.hr>
2015-11-22 23:08:00 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Class: c3
+#
+# This module manages C3 powertools
+#
+class c3 {
+  package { 'c3':
+    ensure   => present,
+    provider => 'rpm',
+    source   => 'http://www.csm.ornl.gov/torc/C3/Software/5.1.2/rpm/c3-5.1.2-1.noarch.rpm',
+  }
+}
